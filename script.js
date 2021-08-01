a412b9f0b8e8eee916b20dc898a64b7178e1f1c6
Mika <mikachiusiwa@gmail.com>
2021-08-01 15:29:29 +0200

    First commit. Created the computerPlay function

---
+++ script.js
@@ -0,0 +1,9 @@
+let play = ["rock", "paper", "scissors"];
+
+function computerPlay(play) {
+  return play[Math.floor(Math.random() * play.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection;
+}
