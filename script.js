67a23df7dab7abb8931f65a0b9ac83fc0fc60c01
Mika <mikachiusiwa@gmail.com>
2021-08-02 13:33:47 +0200

    Completed the basic form of the function for the game

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 let play = ["rock", "paper", "scissors"];
 
-function computerPlay(play) {
+function computerPlay() {
   return play[Math.floor(Math.random() * play.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection;
+  if (playerSelection == "rock" && computerSelection === "scissors") {
+    return "Rock beats scissors! You win! Here's a cookie!";
+  } else if (playerSelection == "rock" && computerSelection === "rock") {
+    return "Rock meets rock! Its a tie! Play again to decide the victor!";
+  } else if (playerSelection == "rock" && computerSelection === "paper") {
+    return "Rock is covered by paper! You have been defeated!Play again for revenge!";
+  } else if (playerSelection == "paper" && computerSelection === "rock") {
+    return "Paper beats rock, you win! Here's a cookie!";
+  } else if (playerSelection == "paper" && computerSelection === "paper") {
+    return "Paper meets paper! Its a tie! Play again to decide the victor!";
+  } else if (playerSelection == "paper" && computerSelection === "scissors") {
+    return "Paper is sliced by scissors! You have been defeated! Play again for revenge";
+  } else if (playerSelection == "scissors" && computerSelection === "paper") {
+    return "Scissors beats rock! You win! Here's a cookie!";
+  } else if (
+    playerSelection == "scissors" &&
+    computerSelection === "scissors"
+  ) {
+    return "Scissors meets scissors! Its a tie! Play again to decide the victor!";
+  } else if (playerSelection == "scissors" && computerSelection === "rock") {
+    return "Scissors is smashed by rock! You have been defeated! Play again for revenge!";
+  } else {
+    return "How the hell did this happen?";
+  }
 }
