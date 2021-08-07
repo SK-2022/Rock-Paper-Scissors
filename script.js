e5980a71b16f057b927fef7de87d40dd283fad4b
Mika <mikachiusiwa@gmail.com>
2021-08-07 20:29:53 +0200

    Add score counter to playRound function.Add winner display to game function

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let play = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let playerScore = 0;
+
 
 function computerPlay(play) {
   return play[Math.floor(Math.random() * play.length)];
@@ -7,24 +10,29 @@ function computerPlay(play) {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase(); 
   if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
     return "Rock beats scissors! You win! Here's a cookie!";
-
+    
   } else if (playerSelection === "rock" && computerSelection === "rock") {
     return "Rock meets rock! Its a tie! Play again to decide the victor!";
 
   } else if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
     return "Rock is covered by paper! You have been defeated!Play again for revenge!";
 
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
     return "Paper beats rock, you win! Here's a cookie!"
 
   } else if (playerSelection === "paper" && computerSelection === "paper") {
     return "Paper meets paper! Its a tie! Play again to decide the victor!";
 
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
     return "Paper is sliced by scissors! You have been defeated! Play again for revenge";
 
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
     return "Scissors beats rock! You win! Here's a cookie!";
 
   } else if (
@@ -32,6 +40,7 @@ function playRound(playerSelection, computerSelection) {
     return "Scissors meets scissors! Its a tie! Play again to decide the victor!";
 
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
     return "Scissors is smashed by rock! You have been defeated! Play again for revenge!";
 
   } else {
@@ -42,10 +51,21 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game(playerSelection, numberOfRounds) {
-  
   for (let i = 0; i < numberOfRounds; i++) {
     let playRoundFunction = playRound(playerSelection, computerPlay(play));
     console.log(playRoundFunction);
   } 
-  return "Game over!"
+  
+  if (playerScore > computerScore) {
+    return "Player Wins! Congratulations!"
+
+  } else if (playerScore < computerScore) {
+    return "Computer Wins! Try again!"
+
+  } else if (playerScore === computerScore) {
+    return "Its a Tie!"
+    
+  } else {
+    return "Whoops, something went wrong!"
+  }
 }
