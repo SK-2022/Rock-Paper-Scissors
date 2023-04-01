659492e3a9d9dc119ebeed8626379e665db7f030
Mika Chiusiwa <mikachiusiwa@gmail.com>
2023-04-01 20:07:47 +0900

    Add logic to prevent game from continuing after one player reaches 5 points and add comments and more spacing to make code more readable

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     let options = ['rock','paper','scissors']
 
-    // Returns element at index 0, 1 or 2 from the options array randomly 
+    // Returns element at index 0, 1 or 2(rock, paper, or scissors) from the options array above randomly 
     return options[Math.floor(Math.random() * 3)]
 }
 
@@ -17,45 +17,46 @@ function playRound (playerSelection, computerSelection){
     computerSelection = getComputerChoice()
     //Gets playerSelection and converts the input to lowercase.
     playerSelection = playerSelection.toLowerCase()
-    //Logic of the game Rock paper scissors
-    if(computerSelection === playerSelection){
-        matchResult.innerText = "Heads lock! It's a tie!!"
-    } else if(computerSelection === 'rock' && playerSelection === "paper"){
-        playerScore++;
-        matchResult.innerText = "Paper beats Rock! You win!"
-    } else if(computerSelection === 'paper' && playerSelection === "scissors"){
-        playerScore++;
-        matchResult.innerText = "Scissors beats Paper! You win!"
-    } else if(computerSelection === 'scissors' && playerSelection === 'rock'){
-        playerScore++;
-        matchResult.innerText = "Rock beats paper! You win"
-    } else if(computerSelection === 'rock' && playerSelection === 'scissors') {
-        computerScore++;
-        matchResult.innerText = "You lose! Scissors beats Paper"
-    } else if(computerSelection === 'paper' && playerSelection === 'rock'){
-        computerScore++;
-        matchResult.innerText = "You lose! Paper beats Rock"
-    } else if(computerSelection === 'scissors' && playerSelection === 'paper') {
-        computerScore++;
-        matchResult.innerText = "You lose! Scissors beats paper"
-    } else {
-        matchResult.innerText = "Input Error"
-    }
-    //Displays scores on the DOM
-    playerScoreCount.innerText = playerScore
-    computerScoreCount.innerText = computerScore
+    
 
-    //Logic to display winner/loser and game continuation as well as stop the function. 
-    if(computerScore < 5 && playerScore < 5) {
-        finalMatchResult.innerText =" The battle continues..."
+     //Logic to display winner/loser and game continuation as well as stop the function. 
+     if(computerScore < 5 && playerScore < 5) {
+
+        //The rules of rock paper scissors
+        if (computerSelection === playerSelection){
+            matchResult.innerText = "Heads lock! It's a tie!!"
+        } else if(computerSelection === 'rock' && playerSelection === "paper"){
+            playerScore++;
+            matchResult.innerText = "Paper beats Rock! You win!"
+        } else if(computerSelection === 'paper' && playerSelection === "scissors"){
+            playerScore++;
+            matchResult.innerText = "Scissors beats Paper! You win!"
+        } else if(computerSelection === 'scissors' && playerSelection === 'rock'){
+            playerScore++;
+            matchResult.innerText = "Rock beats paper! You win"
+        } else if(computerSelection === 'rock' && playerSelection === 'scissors') {
+            computerScore++;
+            matchResult.innerText = "You lose! Scissors beats Paper"
+        } else if(computerSelection === 'paper' && playerSelection === 'rock'){
+            computerScore++;
+            matchResult.innerText = "You lose! Paper beats Rock"
+        } else if(computerSelection === 'scissors' && playerSelection === 'paper') {
+            computerScore++;
+            matchResult.innerText = "You lose! Scissors beats paper"
+        } else {
+            matchResult.innerText = "Input Error"
+        }
+
+    // Winner and loser of the entire game will be determined with these conditions
     } else if(playerScore === 5) {
         finalMatchResult.innerText = "JACKPOT BABY, YOU WON!"
     } else if(computerScore === 5) {
         finalMatchResult.innerText = "UNDER THE BRIDGE FOR YOU! YOU LOST!"
-    } else {
-        finalMatchResult.innerText = "The battle shall continue for eternity"
-    }
+    } 
 
+    //Displays scores on the DOM
+    playerScoreCount.innerText = playerScore
+    computerScoreCount.innerText = computerScore
 }
 
 //Selects the relevant class and gives the ability to make the playround function occur when the images are clicked.
